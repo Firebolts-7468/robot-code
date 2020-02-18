966785c47bcf32f4eb8e2f0fee2655a99efe7130
Scott Mahr <scottmahr@Scotts-MacBook-3.local>
2020-02-18 11:59:50 -0800

    minor commit

--- robot.py
+++ robot.py
@@ -17,41 +17,32 @@ class MyRobot(wpilib.TimedRobot):
         This function is called upon program startup and
         should be used for any initialization code.
         """
+
+        #Get information from network tables
         NetworkTables.initialize()
         self.sd = NetworkTables.getTable("SmartDashboard")
         self.lime = NetworkTables.getTable("limelight")
 
-
-        self.shooterSpeed = 0
-
-
-
-
-
+        #Set up all the motor controllers 
         self.leftMotors = wpilib.Talon(0)
         #self.rearLeft = wpilib.Talon(3)
-        
         #self.left = wpilib.SpeedControllerGroup(self.frontLeft, self.rearLeft)
-
         self.rightMotors = wpilib.Talon(1)
         #self.rearRight = wpilib.Talon(1)
         #self.right = wpilib.SpeedControllerGroup(self.frontRight, self.rearRight)
-
-        self.drive = wpilib.drive.DifferentialDrive(self.leftMotors, self.rightMotors)
-
-        
         self.shooterMotor = wpilib.Talon(4)
-        
         self.indexerMotor = wpilib.Talon(5)
-
         self.intakeMotor = wpilib.Talon(6)
+
+
+        #Set up the drivetrain motors. 
+        self.drive = wpilib.drive.DifferentialDrive(self.leftMotors, self.rightMotors)
         #self.shooterMotor1.setInverted(True)
         self.intakeOn = False
-        self.prevValue = False
-       
+        self.indexerOn = False
+        self.shooterOn = False
 
         self.cycleCount = 0
-
 
         self.joystick = wpilib.XboxController(0)
 
@@ -71,15 +62,9 @@ class MyRobot(wpilib.TimedRobot):
 
 
     def teleopPeriodic(self):
-        # self.drive.driveCartesian(stick['x'], stick['y'], stick['rot'])
-        #speeed for X change to same as Y
+        
 
-        #write a value to a network table that you can see on the driver station 
-
-        #when you push a button, show a value, when you release, show another value
         self.cycleCount+=1
-
-
         #first, let's get some inofrmation from our control station, we default to not moving
         xScale = self.sd.getNumber("xScale",0)
         yScale = self.sd.getNumber("yScale",0)
@@ -94,15 +79,14 @@ class MyRobot(wpilib.TimedRobot):
         indexerState = self.sd.getNumber("indexerState","off")
         shooterState = self.sd.getNumber("shooterState","off")
 
-        
-
-        visionP = self.sd.getNumber("visionP",0)
+        visionP = self.sd.getNumber("visionP",0.025)
 
 
         #here we get the current aiming of the vision system 
         limeTx = self.lime.getNumber("tx",0)
         limeTy = self.lime.getNumber("ty",0)
 
+        #just print out some stuff for debug
         if self.cycleCount%200==0:
             print(str(xScale)+' '+str(yScale)+' '+str(shooterSpeed))
 
@@ -112,32 +96,51 @@ class MyRobot(wpilib.TimedRobot):
         xvalue =  self.joystick.getX(wi.GenericHID.Hand.kRightHand)
         leftTrigger = self.joystick.getTriggerAxis(wi.GenericHID.Hand.kLeftHand)
         rightTrigger = self.joystick.getTriggerAxis(wi.GenericHID.Hand.kRightHand)
-        
+
+        # yvalue =  -self.joystick.getY(1)
+        # xvalue =  self.joystick.getX(0)
+        # leftTrigger = self.joystick.getTriggerAxis(0)
+        # rightTrigger = self.joystick.getTriggerAxis(1)
     
         
-
+        #look to see if the user wants to spin
         if leftTrigger > .1 or rightTrigger >.1:
-            self.drive.curvatureDrive(yvalue/yScale, (-leftTrigger+rightTrigger)*spinScale+steeringTrim, True)
+            self.drive.curvatureDrive(yvalue*yScale, (-leftTrigger+rightTrigger)*spinScale+steeringTrim, True)
+        #Use the A button to spin the robot to look at the target
         elif self.joystick.getAButton():
             if limeTx>5: limeTx=5
             if limeTx<-5: limeTx=-5
             self.drive.curvatureDrive(0, limeTx*visionP, True)
+        #else, just drive normally
         else:
-            self.drive.curvatureDrive(yvalue/yScale, xvalue/xScale+steeringTrim, False)
+            self.drive.curvatureDrive(yvalue*yScale, xvalue*xScale+steeringTrim, False)
         
 
+        #toggle intake using Y button
+        if self.joystick.getYButtonPressed():
+            self.intakeOn = not self.intakeOn
 
-        if intakeState == "on": 
+        #toggle indexer on and off
+        if self.joystick.getXButtonPressed():
+            self.indexerOn = not self.indexerOn
+
+        #toggle shooter on and off
+        if self.joystick.getBButtonPressed():
+            self.shooterOn = not self.shooterOn
+
+
+        #based on input from control panel, and from joystick, turn stuff on and off
+        if intakeState == "on" or (intakeState == "controller" and intakeOn): 
             self.intakeMotor.set(intakeSpeed)
         else:
             self.intakeMotor.set(0)
 
-        if indexerState == "on": 
+        if indexerState == "on" or (indexerState == "controller" and indexerOn): 
             self.indexerMotor.set(indexerSpeed)
         else:
             self.indexerMotor.set(0)
 
-        if shooterState == "on": 
+        if shooterState == "on" or (shooterState == "controller" and shooterOn): 
             self.shooterMotor.set(shooterSpeed)
         else:
             self.shooterMotor.set(0)
@@ -146,18 +149,6 @@ class MyRobot(wpilib.TimedRobot):
         #self.drive.arcadeDrive(yvalue/joystickYScale, xvalue/joystickXScale, squareInputs=True)
 
         #print(yvalue/joystickYScale)
-        
-        #now, let's send info our driver control station 
-
-
-
-
-        #Here, let's deal with the shooter
-
-
-        
-
-
 
         # triggervalue = self.joystick.getTriggerAxis(1)
         # if triggervalue > 0.3:
@@ -168,9 +159,6 @@ class MyRobot(wpilib.TimedRobot):
         #print("self.shooterSpeed")
         #print(shooterSpeed)
         
-
-
-
         #self.sd.putNumber("joystickXScale",joystickXScale)
         #self.sd.putNumber("joystickYScale",joystickYScale)
         #self.sd.putNumber("shooterSpeed",shooterSpeed)
@@ -225,14 +213,6 @@ class MyRobot(wpilib.TimedRobot):
         # self.prevValue = btvalue
 
 
-
-
-        
-  
-       
-
-
-
         # # how to hook up a button to a motor
         # aValue = self.joystick.getSomeButton()
 
@@ -257,14 +237,6 @@ if __name__ == "__main__":
 # https://www.chiefdelphi.com/t/python-drivetrain-pid/161488/2
 # https://frc-pdr.readthedocs.io/en/latest/control/pid_control.html#tuning-methods
 # https://robotpy.readthedocs.io/projects/wpilib/en/latest/_modules/wpilib/interfaces/pidsource.html#PIDSource.getPIDSourceType
-
-
-#Steps to do with students
-  # Show that fused heading changes with robot orientation
-  # turn off all joystick entry, and set a target of 30deg off of starting, and control to it
-
-
-
 
 
 
